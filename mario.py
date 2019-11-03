d90d8e961da3e6c57ba900d4dc453cf00060f321
Giuliana Pham <GPham@midmark.com>
2019-11-02 22:45:35 -0700

    Added mario.mode and image frames for different modes.

--- mario.py
+++ mario.py
@@ -2,12 +2,14 @@ import pygame as pg
 from settings import *
 vec = pg.math.Vector2
 
+
 class Mario(pg.sprite.Sprite):
 
     def __init__(self, game): # centerx, bottom , screen
         super().__init__()
         self.state = STANDING
         self.direction = RIGHT
+        self.mode = NORMAL
         self.walkCount = 0
         self.runCount = 0
         self.slideCount = 0
@@ -18,7 +20,7 @@ class Mario(pg.sprite.Sprite):
         self.game = game
         self.reset()
         self.setup_frames()
-        self.image = self.small_frames[0]
+        self.image = self.frames[0]
         self.rect = self.image.get_rect()
         self.rect.center = (WIDTH / 2, HEIGHT /2)
         self.pos = vec(WIDTH / 2, HEIGHT / 2)
@@ -26,19 +28,56 @@ class Mario(pg.sprite.Sprite):
         self.acc = vec(0, 0)
 
     def setup_frames(self):
-        self.small_frames = []
-        for i in range(12):
-            frame = pg.image.load('images/Mario/{}.png'.format(i))
+        self.frames = []
+        for i in range(16):
+            frame = pg.image.load('images/{}/{}.png'.format(self.mode, i))
+            if self.mode == NORMAL:
+                frame = pg.transform.scale(frame, (30, 40)).convert()
+            elif self.mode == SUPER:
+                frame = pg.transform.scale(frame, (60, 105)).convert()
+            elif self.mode == FIRE:
+                frame = pg.transform.scale(frame, (60, 105)).convert()
+            self.frames.append(frame)
+
+        self.frames_trans1 = []
+        for i in range(16):
+            frame = pg.image.load('images/{}/{}.png'.format(self.mode, i))
             frame = pg.transform.scale(frame, (30, 40)).convert()
-            self.small_frames.append(frame)
+            self.frames_trans1.append(frame)
+
+        self.frames_trans2 = []
+        for i in range(16):
+            frame = pg.image.load('images/{}/{}.png'.format(self.mode, i))
+            frame = pg.transform.scale(frame, (30, 40)).convert()
+            self.frames_trans2.append(frame)
 
         self.left_frames = []
         for x in range(0, 6):
-            self.left_frames.append(self.small_frames[x])
+            self.left_frames.append(self.frames[x])
+        self.left_frames.append(self.frames[12])
+        self.left_frames.append(self.frames[14])
 
         self.right_frames = []
         for x in range(11, 5, -1):
-            self.right_frames.append(self.small_frames[x])
+            self.right_frames.append(self.frames[x])
+        self.right_frames.append(self.frames[13])
+        self.left_frames.append(self.frames[15])
+
+        self.left_frames_trans1 = []
+        for x in range(0, 6):
+            self.left_frames_trans1.append(self.frames_trans1[x])
+
+        self.right_frames_trans1 = []
+        for x in range(11, 5, -1):
+            self.right_frames_trans1.append(self.frames_trans1[x])
+
+        self.left_frames_trans2 = []
+        for x in range(0, 6):
+            self.left_frames_trans2.append(self.frames_trans2[x])
+
+        self.right_frames_trans2 = []
+        for x in range(11, 5, -1):
+            self.right_frames_trans2.append(self.frames_trans2[x])
 
         self.walk_left_frames = []
         self.walk_right_frames = []
@@ -46,17 +85,38 @@ class Mario(pg.sprite.Sprite):
             self.walk_left_frames.append(self.left_frames[x])
             self.walk_right_frames.append(self.right_frames[x])
 
-    def jump(self):
-        self.rect.y += 1
-        hits = pg.sprite.spritecollide(self, self.game.platforms, False)
-        self.rect.y -= 1
-        if hits:
-            self.vel.y -= 15
+        self.walk_left_frames_trans1 = []
+        self.walk_right_frames_trans1 = []
+        for x in range(2, 5):
+            self.walk_left_frames_trans1.append(self.left_frames_trans1[x])
+            self.walk_right_frames_trans1.append(self.right_frames_trans1[x])
 
+        self.walk_left_frames_trans2 = []
+        self.walk_right_frames_trans2 = []
+        for x in range(2, 5):
+            self.walk_left_frames_trans2.append(self.left_frames_trans2[x])
+            self.walk_right_frames_trans2.append(self.right_frames_trans2[x])
+
+    def jump(self):
+        print(self.jumpCount)
+        if self.jumpCount == 0:
+            self.rect.y += 1
+            hits = pg.sprite.spritecollide(self, self.game.platforms, False)
+            self.rect.y -= 1
+            if hits:
+                self.vel.y -= 15
+                self.jumpCount += 1
+        elif self.jumpCount > 0:
+            ####
+            self.vel.y -= 15
+            self.jumpCount += 1
+        if not self.state == JUMPING:
+            self.jumpCount = 0
 
     def reset(self):
         self.state = STANDING
         self.direction = RIGHT
+        self.mode = FIRE
         self.walkCount = 0
         self.runCount = 0
         self.slideCount = 0
@@ -69,8 +129,10 @@ class Mario(pg.sprite.Sprite):
         win.blit(self.image, self.rect)
 
     def update(self):
+        # print(self.state)
         self.acc = vec(0, 0.5)
         keys = pg.key.get_pressed()
+        hold_bottom, hold_left = -1, -1
 
         if self.state == STANDING:
             if keys[pg.K_RIGHT] and keys[pg.K_LEFT]:
@@ -188,6 +250,13 @@ class Mario(pg.sprite.Sprite):
             elif keys[pg.K_LEFT]:
                 self.direction = LEFT
                 self.acc.x += -PLAYER_ACC
+            elif keys[pg.K_SPACE]:
+                self.state = JUMPING
+        if self.state == CROUCHING:
+            test = not keys[pg.K_DOWN]
+            if not keys[pg.K_DOWN]:
+                hold_bottom = self.rect.bottom
+                hold_left = self.rect.left
 
         # APPLY FRICTION
         # Faster you're going the more friction slows you down
@@ -203,33 +272,107 @@ class Mario(pg.sprite.Sprite):
             self.pos.x = WIDTH
 
         self.rect.midbottom = self.pos
-        self.update_frame()
 
-        print(self.state, self.walkCount)
+        if not hold_left == -1:
+            self.update_frame(hold_bottom, hold_left)
+            self.state = STANDING
+        else:
+            self.update_frame()
 
-    def update_frame(self):
+        # print(self.state, self.walkCount)
+
+    def update_frame(self, bottom = None, left = None):
         if self.direction == LEFT:
             if self.state == STANDING:
-                self.image = self.left_frames[5]
+                if self.mode == NORMAL or self.mode == SUPER or self.mode == FIRE:
+                    self.image = self.left_frames[5]
+                elif self.mode == INVINC:
+                    self.image = self.left_frames_trans1[5]
+                    self.mode == INVINC2
+                elif self.mode == INVINC2:
+                    self.image = self.left_frames_trans2[5]
+                    self.mode == INVINC
+                if bottom and left:
+                    self.rect = self.image.get_rect()
+                    self.rect.bottom = bottom
+                    self.rect.left = left
                 self.walkCount = 0
             elif self.state == WALKING:
                 walk = self.walkCount//5
-                print(walk)
-                self.image = self.walk_left_frames[walk]
-                self.walkCount += 1
-                if self.walkCount >= 15:
-                    self.walkCount = 0
+                if self.mode == NORMAL or self.mode == SUPER or self.mode == FIRE:
+                    self.image = self.walk_left_frames[walk]
+                elif self.mode == INVINC:
+                    self.image = self.walk_left_frames_trans1[walk]
+                    self.mode = INVINC2
+                elif self.mode == INVINC2:
+                    self.image = self.walk_left_frames_trans2[walk]
+                    self.mode = INVINC
+            elif self.state == RUNNING:
+                walk = self.walkCount//5
+                if self.mode == NORMAL or self.mode == SUPER or self.mode == FIRE:
+                    self.image = self.walk_left_frames[walk]
+                elif self.mode == INVINC:
+                    self.image = self.walk_left_frames_trans1[walk]
+                    self.mode = INVINC2
+                elif self.mode == INVINC2:
+                    self.image = self.walk_left_frames_trans2[walk]
+                    self.mode = INVINC
+            elif self.state == GLIDING:
+                if self.mode == NORMAL or self.mode == SUPER or self.mode == FIRE:
+                    self.image = self.left_frames[1]
+                elif self.mode == INVINC:
+                    self.image = self.left_frames_trans1[1]
+                    self.mode = INVINC2
+                elif self.mode == INVINC2:
+                    self.image = self.left_frames_trans2[1]
+                    self.mode = INVINC
+            elif self.state == JUMPING:
+                if self.mode == NORMAL or self.mode == SUPER or self.mode == FIRE:
+                    self.image = self.left_frames[0]
+                elif self.mode == INVINC:
+                    self.image = self.left_frames_trans1[0]
+                    self.mode = INVINC2
+                elif self.mode == INVINC2:
+                    self.image = self.left_frames_trans2[0]
+                    self.mode = INVINC
+            elif self.state == CROUCHING:
+                if self.mode == NORMAL or self.mode == SUPER or self.mode == FIRE:
+                    self.image = self.left_frames[6]
+                elif self.mode == INVINC:
+                    self.image = self.left_frames_trans1[6]
+                    self.mode = INVINC2
+                elif self.mode == INVINC2:
+                    self.image = self.left_frames_trans2[6]
+                    self.mode = INVINC
+                self.image = pg.transform.scale(self.image, (60, 70)).convert()
+                bottom = self.rect.bottom
+                left = self.rect.left
+                self.rect = self.image.get_rect()
+                self.rect.bottom = bottom
+                self.rect.left = left
         elif self.direction == RIGHT:
             if self.state == STANDING:
-                self.image = self.right_frames[5]
+                if self.mode == NORMAL or self.mode == SUPER or self.mode == FIRE:
+                    self.image = self.right_frames[5]
+                elif self.mode == INVINC:
+                    self.image = self.right_frames_trans1[5]
+                    self.mode = INVINC2
+                elif self.mode == INVINC2:
+                    self.image = self.right_frames_trans2[5]
+                    self.mode = INVINC
                 self.walkCount = 0
             elif self.state == WALKING:
                 walk = self.walkCount//5
-                print(walk, self.walkCount)
-                self.image = self.walk_right_frames[walk]
-                self.walkCount += 1
-                if self.walkCount >= 15:
-                    self.walkCount = 0
+                if self.mode == NORMAL or self.mode == SUPER or self.mode == FIRE:
+                    self.image = self.walk_right_frames[walk]
+                elif self.mode == INVINC:
+                    self.image = self.walk_right_frames_trans1[walk]
+                elif self.mode == INVINC2:
+                    self.image = self.walk_right_frames_trans2[walk]
+                    self.mode = INVINC
+        self.walkCount += 1
+        if self.walkCount >= 15:
+            self.walkCount = 0
 
 
 
